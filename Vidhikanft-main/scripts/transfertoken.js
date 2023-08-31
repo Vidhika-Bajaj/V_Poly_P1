a8316ac84b30f726f1d50af738627fe46e5f43d3
Vidhika Bajaj <136812275+Vidhika-Bajaj@users.noreply.github.com>
2023-09-01 02:52:59 +0530

    Update transfertoken.js

--- Vidhikanft-main/scripts/transfertoken.js
+++ Vidhikanft-main/scripts/transfertoken.js
@@ -3,7 +3,7 @@ const VidhikaNFTJSON = require("../artifacts/contracts/VidhikaNFT.sol/VidhikaNFT
 
 const contract_address = "0x616E9ed7E928fc95E68555bB94809e178D6C3AA9"; // Replace with the actual contract address
 const VidhikaNFTABI = VidhikaNFTJSON.abi;
-const walletAddress = "0x3Fc5fb342Aec4773ce3C4FB70FC357f22E2c1dE1"; // Replace with your wallet address
+const walletAddress = "0x77E930D39F67A75E5054bF73a92ffC02c98d9cf2"; // Replace with your wallet address
 
 const fxRootContractABI = require("../fxRootContractABI.json");
 const fxERC1155RootAddress = "0x883805323E06f7BFf1C8Df676acD592493820059"; // Replace with actual address
